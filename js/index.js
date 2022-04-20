3a48fd904a7f5bc035bd1eb30673907c7c0f9e86
maltseva-k <k.obydennova@gmail.com>
2022-04-20 14:34:41 +0300

    index.js refactoring

--- js/index.js
+++ js/index.js
@@ -1,87 +1,51 @@
 
 import Swiper from 'https://unpkg.com/swiper@8/swiper-bundle.esm.browser.min.js'
-
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'vertical',
-    loop: false,
-    /*cssMode: true,*/
-    mousewheel: true,
-    speed: 2000,
-    initialSlide: 0,
-
-    // If we need pagination
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true
-    },
-
-    // Navigation arrows
-    navigation: {
-        nextEl: '.swiper-button'
-    }
-});
-swiper.on('activeIndexChange', function () {
-    if(swiper.activeIndex === 0) {
-        circle1.classList.remove('animation-circle-1', 'animation-2-circle-1', 'animation-3-circle-1', 'animation-4-circle-1')
-        circle2.classList.remove('animation-circle-2', 'animation-2-circle-2', 'animation-3-circle-2', 'animation-4-circle-2')
-        circle3.classList.remove('animation-circle-3', 'animation-2-circle-3', 'animation-3-circle-3', 'animation-4-circle-3')
-        circle4.classList.remove('animation-circle-4', 'animation-2-circle-4', 'animation-3-circle-4', 'animation-4-circle-4')
-        button.classList.remove('hidden')
-    }
-
-    if(swiper.activeIndex === 1) {
-        circle1.classList.remove('animation-circle-1', 'animation-2-circle-1', 'animation-3-circle-1', 'animation-4-circle-1')
-        circle2.classList.remove('animation-circle-2', 'animation-2-circle-2', 'animation-3-circle-2', 'animation-4-circle-2')
-        circle3.classList.remove('animation-circle-3', 'animation-2-circle-3', 'animation-3-circle-3', 'animation-4-circle-3')
-        circle4.classList.remove('animation-circle-4', 'animation-2-circle-4', 'animation-3-circle-4', 'animation-4-circle-4')
-        circle1.classList.add('animation-circle-1')
-        circle2.classList.add('animation-circle-2')
-        circle3.classList.add('animation-circle-3')
-        circle4.classList.add('animation-circle-4')
-        button.classList.remove('hidden')
-    }
-    if(swiper.activeIndex === 2) {
-        circle1.classList.remove('animation-circle-1', 'animation-2-circle-1', 'animation-3-circle-1', 'animation-4-circle-1')
-        circle2.classList.remove('animation-circle-2', 'animation-2-circle-2', 'animation-3-circle-2', 'animation-4-circle-2')
-        circle3.classList.remove('animation-circle-3', 'animation-2-circle-3', 'animation-3-circle-3', 'animation-4-circle-3')
-        circle4.classList.remove('animation-circle-4', 'animation-2-circle-4', 'animation-3-circle-4', 'animation-4-circle-4')
-        circle1.classList.add('animation-2-circle-1')
-        circle2.classList.add('animation-2-circle-2')
-        circle3.classList.add('animation-2-circle-3')
-        circle4.classList.add('animation-2-circle-4')
-        button.classList.remove('hidden')
-    }
-    if(swiper.activeIndex === 3) {
-        circle1.classList.remove('animation-circle-1', 'animation-2-circle-1', 'animation-3-circle-1', 'animation-4-circle-1')
-        circle2.classList.remove('animation-circle-2', 'animation-2-circle-2', 'animation-3-circle-2', 'animation-4-circle-2')
-        circle3.classList.remove('animation-circle-3', 'animation-2-circle-3', 'animation-3-circle-3', 'animation-4-circle-3')
-        circle4.classList.remove('animation-circle-4', 'animation-2-circle-4', 'animation-3-circle-4', 'animation-4-circle-4')
-        circle1.classList.add('animation-3-circle-1')
-        circle2.classList.add('animation-3-circle-2')
-        circle3.classList.add('animation-3-circle-3')
-        circle4.classList.add('animation-3-circle-4')
-        button.classList.remove('hidden')
-    }
-    if(swiper.activeIndex === 4) {
-        circle1.classList.remove('animation-circle-1', 'animation-2-circle-1', 'animation-3-circle-1', 'animation-4-circle-1')
-        circle2.classList.remove('animation-circle-2', 'animation-2-circle-2', 'animation-3-circle-2', 'animation-4-circle-2')
-        circle3.classList.remove('animation-circle-3', 'animation-2-circle-3', 'animation-3-circle-3', 'animation-4-circle-3')
-        circle4.classList.remove('animation-circle-4', 'animation-2-circle-4', 'animation-3-circle-4', 'animation-4-circle-4')
-        circle1.classList.add('animation-4-circle-1')
-        circle2.classList.add('animation-4-circle-2')
-        circle3.classList.add('animation-4-circle-3')
-        circle4.classList.add('animation-4-circle-4')
-        button.classList.add('hidden')
-    }
-
-});
-
 const circle1 = document.querySelector('.circle-1')
 const circle2 = document.querySelector('.circle-2')
 const circle3 = document.querySelector('.circle-3')
 const circle4 = document.querySelector('.circle-4')
 const button = document.querySelector('.swiper-button')
+
+
+const swiper = new Swiper('.swiper', {
+    direction: 'vertical',
+    loop: false,
+    mousewheel: true,
+    speed: 2000,
+    initialSlide: 0,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+    },
+    navigation: {
+        nextEl: '.swiper-button'
+    }
+})
+
+function addClassIndex(index) {
+    circle1.classList.add(`animation-${index}-circle-1`)
+    circle2.classList.add(`animation-${index}-circle-2`)
+    circle3.classList.add(`animation-${index}-circle-3`)
+    circle4.classList.add(`animation-${index}-circle-4`)
+}
+
+function removeClassIndex() {
+    for (let i=1; i<=4; i++) {
+        circle1.classList.remove(`animation-${i}-circle-1`)
+        circle2.classList.remove(`animation-${i}-circle-2`)
+        circle3.classList.remove(`animation-${i}-circle-3`)
+        circle4.classList.remove(`animation-${i}-circle-4`)
+    }
+    button.classList.remove('hidden')
+}
+
+swiper.on('activeIndexChange', function () {
+    removeClassIndex()
+    addClassIndex(swiper.activeIndex)
+    if(swiper.activeIndex === 4) {
+        button.classList.add('hidden')
+    }
+})
 
 
 
